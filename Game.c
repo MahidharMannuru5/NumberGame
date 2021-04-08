e7e614ce19775dfbcafc245d04bcb1144bc3f0b9
MahidharMannuru5 <81514511+MahidharMannuru5@users.noreply.github.com>
2021-04-08 10:21:25 +0530

    Create Game.c

---
+++ Game.c
@@ -0,0 +1,24 @@
+#include  <stdio.h>
+#include<conio.h>
+
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int number1,number2,Random,guess;
+  
+  printf("Enter two numbers and enter your guess number");
+  scanf("%d %d %d",&number1,&number2,&guess);
+Random=number1*number2;
+if (Random=='guess')
+{
+	printf("you won");
+	
+}
+else{
+	printf("you lost");
+}
+
+ 
+}
